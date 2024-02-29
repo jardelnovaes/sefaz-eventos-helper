3649851efe1039de5498cac8be5614db65d1d40a
jardelnovaes <jardelnovaes@hotmail.com>
2024-02-29 19:44:16 -0300

    release process

---
+++ resources/build-helper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+opt=$1
+
+prepareRelease() {
+  echo "Preparing Release"
+  mvn -B build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+  git add . && git commit -m "[release-version] prepare release"
+}
+
+nextDevelopmentVersion() {
+  echo "Preparing Next Development Version"
+  mvn -B build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT versions:commit
+  git add . && git commit -m "[release-version] prepare for next development iteration"
+}
+
+case $opt in
+  "prepare-release") prepareRelease ;;
+  "next-dev-version") nextDevelopmentVersion ;;
+  *) echo -e "Options:\n\t[ prepare-release | next-dev-version ]"; exit 1 ;;
+esac
